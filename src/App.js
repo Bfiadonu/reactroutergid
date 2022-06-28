91024e4266d708ee2d1d24869c199c35bd15f765
bfiadonu <bfiadonu@gmail.com>
2022-06-28 13:22:44 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Basics from './components/Basics'
+import Types from './components/Types'
+import Equipment from './components/Equipment'
+import Where from './components/Where'
+import Safety from './components/Safety'
+import Footer from './components/Footer'
+import Navigation from './components/Navigation'
+import Header from './components/Header'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <Header />
+
+      <BrowserRouter>
+    <div>
+     <Navigation />
+      <Routes>
+        <Route exact path="/" element={<Basics/>} />
+        <Route path="/types" element={<Types/>} />
+        <Route path="/equipment" element={<Equipment/>} />
+        <Route path="/where" element={<Where/>} />
+        <Route path="/safety" element={<Safety/>} />
+      </Routes>
+    </div>
+  </BrowserRouter>
+  <Footer />
     </div>
   );
 }
